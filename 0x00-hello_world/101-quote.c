b65d2cbae05a49429231b79502ccc5c82f3f6188
Han Silver227 <goldenamximus44@gmail.com>
2022-09-08 15:14:21 -0700

    fix fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * *main - Entry point
  * *
  * *Description: 'and that piece of art is useful
- * "-Dora Korpar, 2015-10-19"
+ * "-Dora Korpar, 2015-10-19'
  * *
  *  *Return: Always 0 (Success)
  */
