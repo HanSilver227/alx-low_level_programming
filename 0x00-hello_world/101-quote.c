672ae431692d7fe33fd3915501cc9f8f9245d149
Han Silver227 <goldenamximus44@gmail.com>
2022-09-08 15:10:55 -0700

    fixed?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Description: 'and that piece of art is useful
+ * *main - Entry point
+ * *
+ * *Description: 'and that piece of art is useful
  * "-Dora Korpar, 2015-10-19"
- *
- * Return: Always 0 (Success)
+ * *
+ *  *Return: Always 0 (Success)
  */
 
 int main(void)
