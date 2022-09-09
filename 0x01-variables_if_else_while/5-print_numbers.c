64410c99732358bce6716f38ebe6a3bed281a54c
Han Silver227 <goldenamximus44@gmail.com>
2022-09-09 15:19:36 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry block
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+		printf("%d\n", numbers);
+	return (0);
+}
