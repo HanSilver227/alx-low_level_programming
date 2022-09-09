2b3f82e9c6c744f098132c498774f5827b63f86b
Han Silver227 <goldenamximus44@gmail.com>
2022-09-09 15:26:47 -0700

    fixed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int numbers;
 
-	for (numbers = 1; numbers < 10; numbers++)
+	for (numbers = 0; numbers < 10; numbers++)
 	printf("%d", numbers);
-	putchar('\n');	
+	putchar('\n');
 	return (0);
 }
