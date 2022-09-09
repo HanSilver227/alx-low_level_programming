69bfe89c54a194953c7890369da32ccb1e38c3bd
Han Silver227 <goldenamximus44@gmail.com>
2022-09-09 15:59:19 -0700

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry block
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		putchar(number + '0');
+		if (number <  9)
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
