4253f040b5762fafcd895e1704cfcad18572ba9a
Han Silver227 <goldenamximus44@gmail.com>
2022-09-09 14:49:54 -0700

    Lowercase and Uppercase letters

---
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Entry block
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets;
+	char CAPS;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		putchar(alphabets);
+	for (CAPS - 'A'; CAPS <= 'Z'; CAPS++)
+		putchar(CAPS);
+	putchar('\n');
+	return (0);
+}
