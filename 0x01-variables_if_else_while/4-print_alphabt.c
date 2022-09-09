de44f5e05064de58bc4a38b0fe4390282c60fc9d
Han Silver227 <goldenamximus44@gmail.com>
2022-09-09 15:10:51 -0700

    fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,7 @@ int main(void)
 
 	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 	{
-		if (alphabets == 'q' && alphabets == 'e')
-			continue
-		else
+		if (alphabets != 'q' && alphabets != 'e')
 			putchar(alphabets);
 	putchar('\n');
 	return (0);
