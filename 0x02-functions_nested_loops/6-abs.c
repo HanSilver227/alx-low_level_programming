8f970728d37de653ce4f1f9b91de8c5a93ff14ba
Han Silver227 <goldenamximus44@gmail.com>
2022-09-13 21:35:22 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ *
+ * @n: integer to represent
+ *
+ * Return: int or int times negative
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
