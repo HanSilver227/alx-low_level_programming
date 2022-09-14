636de4734426e88ec8e963a17d86e4536f166fff
Han Silver227 <goldenamximus44@gmail.com>
2022-09-14 13:58:08 -0700

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "6-abs.c"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: integer to represent
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
