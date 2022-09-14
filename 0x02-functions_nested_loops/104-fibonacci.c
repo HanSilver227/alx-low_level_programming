c1092445cd583b3639c7621ab71c53ca91c3a71b
Han Silver227 <goldenamximus44@gmail.com>
2022-09-14 16:30:05 -0700

    Fibonacci - resolved??

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,25 +7,29 @@
 
 int main(void)
 {
-	int counter = 2;
+	unsigned long int i, j, jk1, k2, k1, k2;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
+	j = 1;
+	k = 2;
+	printf("%lu", j);
 
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
+	for (i = 1; i < 91; i++)
 	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
-		{
-			printf(", ");
-		}
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+	for (i = 92; i < 99; ++i)
+	{
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
 	printf("\n");
 	return (0);
