d7e80cb56ee5edf557cd89cc38f83059dad98e59
Han Silver227 <goldenamximus44@gmail.com>
2022-09-13 22:09:07 -0700

    Times table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 		for (j = 0; j <= 59; j++)
 		{
 			_putchar(i / 10 + '0');
-			_putchar(i % 10 % '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
 			_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
