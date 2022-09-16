568f3e4052bda4c14e446cac4af45a60d74bcb5f
Han Silver227 <goldenamximus44@gmail.com>
2022-09-16 15:26:24 -0700

    Checking for prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+long x, y;
+
+x = 612852475143;
+for (y = 2; x > y; y++)
+{
+while (x % y == 0)
+{
+x = x / y;
+}
+printf("%lu", y);
+putchar('\n');
+return (0);
+}
