e5d4efa1ce8dd11d7415c38791745d450e9aae8e
Han Silver227 <goldenamximus44@gmail.com>
2022-09-16 16:19:56 -0700

    Avoiding Infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+/*
+ * while (i < 10)
+ *{
+ *putchar(i);
+ *}
+ */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
