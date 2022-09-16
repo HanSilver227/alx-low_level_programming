c4797e60782fab7621c862b2e553dd79990e6da4
Han Silver227 <goldenamximus44@gmail.com>
2022-09-16 15:51:28 -0700

    resolved!

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,17 +7,19 @@
 
 int main(void)
 {
-long x, y;
 
-x = 612852475143;
-for (y = 2; x > y; y++)
-{
-while (x % y == 0)
-{
-x = x / y;
-}
-printf("%lu", y);
-putchar('\n');
-return (0);
-}
+	long x, y;
+
+	x = 612852475143;
+
+	for (y = 2; x > y; y++)
+	{
+		while (x % y == 0)
+		{
+			x = x / y;
+		}
+	}
+	printf("%lu", y)
+	putchar('\n');
+	return (0);
 }
