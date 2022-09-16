0575d959e31ce60bc25107a59bc0fb5fe30893f5
Han Silver227 <goldenamximus44@gmail.com>
2022-09-16 16:31:45 -0700

    largest number & main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main -entry point
+ * Return: 0 (Success)
  */
 
 int main(void)
@@ -10,5 +10,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
