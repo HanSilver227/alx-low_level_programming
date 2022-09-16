ea56e2328142cff545439b6ebe037a904f1444ff
Han Silver227 <goldenamximus44@gmail.com>
2022-09-15 20:22:45 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - entry point
+ *
+ * @n: integer to represent
+ */
+
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
