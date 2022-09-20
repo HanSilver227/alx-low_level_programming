6e85927a881f9817f871b377c7e517560539637f
Han Silver227 <goldenamximus44@gmail.com>
2022-09-20 15:33:50 -0700

    Swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main."h
+
+/**
+ * swap_int - Entry point
+ *
+ * @a: take an int
+ *
+ * @b: take an int
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+
+	*a = *b;
+	*b = i;
+}
