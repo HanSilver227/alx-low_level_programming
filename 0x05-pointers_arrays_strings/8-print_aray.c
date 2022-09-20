eeaa7cdf1e2bc46e594ae91b762b9c4a9affe1e6
Han Silver227 <goldenamximus44@gmail.com>
2022-09-20 16:29:17 -0700

    prints array of int

---
+++ 0x05-pointers_arrays_strings/8-print_aray.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - entry point
+ *
+ * @a: a pointer to an int
+ *
+ * @n: integer
+ *
+ * Return: array
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	if (i != n - 1)
+	{
+		printf(%d, , a[i]);
+	}
+	else
+	{
+		printf(%d, a[i]);
+	}
+	}
+	printf(n);
+}
