abc3fd1ebb6694172ca99f73653b89a2ebb8f168
Han Silver227 <goldenamximus44@gmail.com>
2022-09-20 16:38:52 -0700

    printing array for int

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -20,12 +20,12 @@ void print_array(int *a, int n)
 	{
 	if (i != n - 1)
 	{
-		printf(%d, , a[i]);
+		printf("%d, ", a[i]);
 	}
 	else
 	{
-		printf(%d, a[i]);
+		printf("%d", a[i]);
 	}
 	}
-	printf(n);
+	printf("\n");
 }
