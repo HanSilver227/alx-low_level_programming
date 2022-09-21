42963aec866d50adf3d43e8e13d52d17526eb4cf
Han Silver227 <goldenamximus44@gmail.com>
2022-09-21 15:01:09 -0700

    Encoding strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,20 @@
 
 char *leet(char *str)
 {
-	int i, j;
-
 	char s[] = a4e3o0t7l1;
+	int i;
+	int j;
 
-	i = 0;
-	j = 0;
-
-	while (str[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j += 2)
+		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (str[i] == s[j] || str[i] == s[j] - 32)
+			if (str[i] == s[j])
 			{
 				str[i] = s[j + 1];
 				break;
 			}
 		}
-		i++;
 	}
 	return (str);
 }
