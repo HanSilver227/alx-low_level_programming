bc1ead1ae5bd826e278029be27cedd9b0f2a1a42
Han Silver227 <goldenamximus44@gmail.com>
2022-09-21 14:42:02 -0700

    Capitalize string words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *cap_string - entry point
+ * @str: string
+ * Return: string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 1;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
+	while (str[i] != '\0')
+	{
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t'
+			|| str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
+			|| str[i - 1] == ',' || str[i - 1] == '!' || str[i - 1] == '?'
+			|| str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
+			|| str[i - 1] == '{' || str[i - 1] == '}')
+				&& (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
