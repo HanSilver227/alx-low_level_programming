1a5ee528d71377a6b0c423346168601546b6c60f
Han Silver227 <goldenamximus44@gmail.com>
2022-10-13 20:05:31 -0700

    {Print all - sub printf function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,96 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+* print_char - prints a char
+* @vargs: argument
+* Return: void
+*
+*/
+
+void print_char(va_list vargs)
+{
+	printf("%c", va_arg(vargs, int));
+}
+
+/**
+* print_integer - prints an integer
+* @vargs: argument
+* Return: void
+*/
+
+void print_integer(va_list vargs)
+{
+	printf("%d", va_arg(vargs, int));
+}
+
+/**
+* print_float - prints a float
+* @vargs: argument
+* Return: void
+*/
+void print_float(va_list vargs)
+{
+	printf("%f", va_arg(vargs, double));
+}
+
+/**
+* print_string - prints a string
+* @vargs: argument
+* Return: void
+*/
+
+void print_string(va_list vargs)
+{
+	char *ch;
+
+	ch = va_args(vargs, char*);
+	if (ch == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", ch);
+}
+
+/**
+ * print_all - a function that prints all arguments
+ * @format: the format
+ * Return: nothing
+ */
+
+void print_all(const char *const format, ...)
+{
+	int i, j;
+	char *str = "";
+	va_list vargs;
+
+	form p[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL},
+	};
+	va_start(args, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (p[j].f)
+		{
+			if (*(p[j].f) == format[i])
+			{
+				printf("%s", str);
+				p[j].func(args);
+				str = ",";
+			}
+			j++
+		}
+		i++
+	}
+	va_end(args);
+	printf("\n");
+}
