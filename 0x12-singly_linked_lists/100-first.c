e7eadf824bdbd0dfc8519821400b6605633ad5c9
Han Silver227 <goldenamximus44@gmail.com>
2022-10-20 19:15:03 -0700

    Printing string before 'main'

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * printfirst - printing 'You\'re beat!'
+ * Return: Sucess. Always 0
+ */
+
+void printFirst(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
